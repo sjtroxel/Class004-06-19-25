4312fc1d6ac82910c53dca45ebfb8611d5429eae
sjtroxel <sjtroxel@alumni.iu.edu>
2025-06-17 20:42:32 -0500

    i don't like the # . . . in the readings it makes it hard to understand

---
+++ countries_of_the_world_cli/lib/user.rb
@@ -0,0 +1,33 @@
+require "bcrypt"
+
+class User
+  attr_accessor :username, :password
+
+  @@users = []
+  def initialize(username, password)
+    @username = username
+    @password = BCrypt::Password.create(password)
+    @@users << self
+  end
+
+  def self.authenticate(username, password)
+    user = User.find_by_username(username)
+
+    if user && user.password == password
+      return user
+    else
+      return nil
+    end
+  end
+
+  def self.all
+    @@users
+  end
+
+  def self.find_by_username(username)
+    user = all.find do |user|
+      user.username == username
+    end
+    user
+  end
+end
