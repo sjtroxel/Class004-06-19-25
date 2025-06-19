9d906aafccc3824fe08453652016724e504e7128
sjtroxel <sjtroxel@alumni.iu.edu>
2025-06-18 21:18:20 -0500

    added new users to the json file!!

--- usa-covid-19-tracker/lib/user.rb
+++ usa-covid-19-tracker/lib/user.rb
@@ -1,4 +1,5 @@
 require 'bcrypt'
+require 'json'
 
 class User
   attr_accessor :username, :password
@@ -8,7 +9,8 @@ class User
   def initialize(username, password)
     @username = username
     @password = BCrypt::Password.create(password)
-
+      # add the user to an external file
+    User.store_credentials(self)
     @@users << self
   end
 
@@ -31,5 +33,22 @@ class User
     user = all.find do |user|
       user.username == username
     end
+  end
+
+  def self.store_credentials(user)
+    # Step 1: Create a file if it doesn't exist
+    file_path = "users.json"
+    unless File.exist?(file_path)
+      File.open(file_path, "w") { |file| file.write(JSON.generate([])) }
+    end
+
+    # Step 2: Add user to the file
+    file = File.read(file_path)
+    users_data = JSON.parse(file)
+
+    users_data << { username: user.username, password: user.password }
+
+    File.open(file_path, "w") { |file| file.write(JSON.generate(users_data)) }
+
   end
 end
